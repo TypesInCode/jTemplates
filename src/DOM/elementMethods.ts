7138201b48e696f60d18502fd9b2f324b5fb5f4f
TypesInCode <landrumjc@gmail.com>
2017-12-07 17:02:51 -0500

    added img ElementMethod

--- src/DOM/elementMethods.ts
+++ src/DOM/elementMethods.ts
@@ -2,3 +2,4 @@ import { ElementMethod, element } from "./elements";
 
 export var div: ElementMethod = element.bind(null, "div");
 export var span: ElementMethod = element.bind(null, "span");
+export var img: ElementMethod = element.bind(null, "img");
