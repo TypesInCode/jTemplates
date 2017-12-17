ded56c9e874931ccdcf6e23e907460a90165fa1a
TypesInCode <landrumjc@gmail.com>
2017-12-17 02:03:30 -0500

    added element methods

--- src/DOM/elementMethods.ts
+++ src/DOM/elementMethods.ts
@@ -5,3 +5,5 @@ export var span: ElementMethod = element.bind(null, "span");
 export var img: ElementMethod = element.bind(null, "img");
 export var video: ElementMethod = element.bind(null, "video");
 export var source: ElementMethod = element.bind(null, "source");
+export var option: ElementMethod = element.bind(null, "option");
+export var select: ElementMethod = element.bind(null, "select");
