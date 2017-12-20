6c0c5626f595918a504609270d04ae27f386627a
TypesInCode <landrumjc@gmail.com>
2017-12-19 23:56:13 -0500

    added input method

--- lib/DOM/elementMethods.d.ts
+++ lib/DOM/elementMethods.d.ts
@@ -4,5 +4,6 @@ export declare var span: ElementMethod;
 export declare var img: ElementMethod;
 export declare var video: ElementMethod;
 export declare var source: ElementMethod;
+export declare var input: ElementMethod;
 export declare var option: ElementMethod;
 export declare var select: ElementMethod;
