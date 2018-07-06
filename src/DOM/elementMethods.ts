df94202db9eeb9ef820fdbfc366dd302e82c765b
TypesInCode <landrumjc@gmail.com>
2018-07-06 10:09:36 -0400

    added element methods

--- src/DOM/elementMethods.ts
+++ src/DOM/elementMethods.ts
@@ -1,6 +1,7 @@
 import { ElementMethod, element, ValueFunction } from "./elements";
 
 export var br: ElementMethod = element.bind(null, "br");
+export var b: ElementMethod = element.bind(null, "b");
 export var div: ElementMethod = element.bind(null, "div");
 export var span: ElementMethod = element.bind(null, "span");
 export var img: ElementMethod = element.bind(null, "img");
@@ -9,3 +10,6 @@ export var source: ElementMethod = element.bind(null, "source");
 export var input: ElementMethod = element.bind(null, "input");
 export var option: ElementMethod = element.bind(null, "option");
 export var select: ElementMethod = element.bind(null, "select");
+export var h1: ElementMethod = element.bind(null, "h1");
+export var h2: ElementMethod = element.bind(null, "h2");
+export var h3: ElementMethod = element.bind(null, "h3");
