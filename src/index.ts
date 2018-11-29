7aabd75e46ed529c84592997e32a96839559af87
TypesInCode <landrumjc@gmail.com>
2018-11-28 20:04:21 -0500

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { Template, BindingDefinition } from "./template";
+import { Template } from "./template";
 import { ObjectStore, Value } from './ObjectStore/objectStore';
 import { div, span, input, b, a, br, img, video, source, option, select, h1, h2, h3 } from "./DOM/elements";
 
-// import { ProxyObservable, Value } from "./ProxyObservable/proxyObservable";
-
-export { Template, ObjectStore, BindingDefinition, Value, div, span, input, b, a, br, img, video, source, option, select, h1, h2, h3 };
+export { Template, ObjectStore, Value, div, span, input, b, a, br, img, video, source, option, select, h1, h2, h3 };
