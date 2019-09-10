bcda215b3d0a5d92adf1bddc4ce357384284c651
TypesInCode <landrumjc@gmail.com>
2019-09-10 17:49:04 -0400

    moved namespace to const

--- src/DOM/svgElements.ts
+++ src/DOM/svgElements.ts
@@ -1,12 +1,14 @@
 import { TemplateFunction } from "../Template/template";
 import { TemplateDefinition, BindingDefinition, ChildrenOr } from "../Template/template.types";
 
+const svgNs = "http://www.w3.org/2000/svg";
+
 export function svg<P>(templateDefinition?: TemplateDefinition<P>, children?: ChildrenOr<P>): BindingDefinition<any, any> {
-    return TemplateFunction("svg", "http://www.w3.org/2000/svg", templateDefinition, children);
+    return TemplateFunction("svg", svgNs, templateDefinition, children);
 }
 export function g<P>(templateDefinition?: TemplateDefinition<P>, children?: ChildrenOr<P>): BindingDefinition<any, any> {
-    return TemplateFunction("g", "http://www.w3.org/2000/svg", templateDefinition, children);
+    return TemplateFunction("g", svgNs, templateDefinition, children);
 }
 export function circle<P>(templateDefinition?: TemplateDefinition<P>, children?: ChildrenOr<P>): BindingDefinition<any, any> {
-    return TemplateFunction("circle", "http://www.w3.org/2000/svg", templateDefinition, children);
+    return TemplateFunction("circle", svgNs, templateDefinition, children);
 }
