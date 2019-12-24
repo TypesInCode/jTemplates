fdc50b296a6cb1492787c87844add2e7ae4daf28
TypesInCode <landrumjc@gmail.com>
2019-12-24 17:58:21 -0500

    debug index

---
+++ src/index.debug.ts
@@ -0,0 +1,24 @@
+import { StoreSync, Scope } from "./Store";
+
+async function Test() {
+    var store = new StoreSync({}, (val) => val._id);
+    var query = store.Query((reader) => {
+      var obj = reader.Get<{ _id: string, property: string }>("very-unique-id");
+      return obj && obj.property;
+    });
+
+    var setFired = false;
+    query.addListener("set", () => {
+      setFired = true;
+    });
+
+    await store.Action(async (reader, writer) => {
+      await writer.Write({ _id: "very-unique-id", property: "property value" });
+    });
+    console.log(setFired)
+    console.log(query.Value);
+    store.Destroy();
+    query.Destroy();
+}
+
+Test();
