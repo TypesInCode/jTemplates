ac2ddbf066825835438f2ddc7650eba3f4eb0f28
TypesInCode <landrumjc@gmail.com>
2019-12-24 17:59:19 -0500

    added store tests

--- test/store-spec.ts
+++ test/store-spec.ts
@@ -1,5 +1,60 @@
 /// <reference path="../node_modules/@types/mocha/index.d.ts" />
 
+import { StoreSync, Scope } from "../src/Store";
+import * as chai from "chai";
+import { stringify } from "querystring";
+const expect = chai.expect;
+
+describe("Store (Sync)", () => {
+  it("Basic Test", async () => {
+    var store = new StoreSync({ firstProperty: 'old value', secondProperty: 'second old value' });
+    expect(store.Root.Value.firstProperty).to.equal('old value');
+    expect(store.Root.Value.secondProperty).to.equal('second old value');
+    store.Destroy();
+  });
+  it("Basic Scope - Update", async () => {
+    var store = new StoreSync({ firstProperty: 'old value', secondProperty: 'second old value' });
+    var scope = new Scope(() => `${store.Root.Value.firstProperty} ${store.Root.Value.secondProperty}`);
+    expect(scope.Value).to.equal('old value second old value');
+
+    var setFired = false;
+    scope.addListener('set', () => {
+      setFired = true;
+    });
+
+    await store.Update({
+      firstProperty: 'new value',
+      secondProperty: 'second new value'
+    });
+    
+    expect(setFired).to.be.true;
+    expect(scope.Value).to.equal('new value second new value');
+    
+    scope.Destroy();
+    store.Destroy();
+  });
+  it("Basic ID", async () => {
+    var store = new StoreSync({}, (val) => val._id);
+    var query = store.Query((reader) => {
+      var obj = reader.Get<{ _id: string, property: string }>("very-unique-id");
+      return obj && obj.property;
+    });
+
+    var setFired = false;
+    query.addListener("set", () => {
+      setFired = true;
+    });
+
+    await store.Action(async (reader, writer) => {
+      await writer.Write({ _id: "very-unique-id", property: "property value" });
+    });
+    expect(setFired).to.equal(true);
+    expect(query.Value).to.be.equal("property value");
+    store.Destroy();
+    query.Destroy();
+  });
+});
+
 /* import { Store } from "../src/Store/sync/store";
 import * as chai from "chai";
 const expect = chai.expect;
