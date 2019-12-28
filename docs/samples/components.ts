f3fb25ba2ba5d6ac41a1c109324abb87925cee29
TypesInCode <landrumjc@gmail.com>
2019-12-28 00:49:05 -0500

    docs changes

--- docs/samples/components.ts
+++ docs/samples/components.ts
@@ -1,87 +1,56 @@
-// 'Component' is the base type required for components. 
-// 'NodeRef' is the base type for all types used in template definitions.
 import { Component, NodeRef } from 'j-templates';
-// div, h1 are template element functions
-import { div, h1 } from 'j-templates/DOM';
+import { div, h1, input } from 'j-templates/DOM';
 
-// Type definition for the data this 'Component' can receive from a 
-// parent 'Component'
 interface IData {
     title: string;
 }
 
-// Type definition for the template definitions this 'Component' accepts 
-// from a parent 'Component'
 interface ITemplates {
     body: () => NodeRef | NodeRef[];
 }
 
-// Type definition for the events this `Component` supports. A `Component` does 
-// not support standard DOM events and must define the events that can be 
-// listened for by a parent `Component`. The type of the interface property 
-// defines the type of the parameter passed when the event fires. Void indicates 
-// no parameter is passed to the event.
 interface IEvents {
     headerClick: void;
 }
 
-// Class definition extending Component. Component accepts three optional 
-// generic types defining: Data, Templates, and Events.
 class MyComponent extends Component<IData, ITemplates, IEvents> {
-    // Store definition
-    _store = new StoreSync({ headerToggled: false });
-    // Scope definition
-    _headerClassScope = this._store.Root.Scope(root => {
+    _state = new StoreSync({ headerToggled: false });
+    _headerClassScope = this._state.Root.Scope(root => {
         return root.headerToggled ? 'toggled' : 'not-toggled';
     });
 
-    // getter to clean up accessing Scope value
     get HeaderClass() {
         return this._headerClassScope.Value;
     }
 
-    // Component.Template definition function
     public Template() {
         return [
-            // h1 element
             h1({
-                // dynamic props binding. If a function is not used then a static
-                // binding is created.
-                // Type maps 1to1 to Partial<HTMLElement> type
                 props: () => ({
                     className: this.HeaderClass
                 }),
-                // static on binding
-                // IEvents type
                 on: { 
                     click: () => {
-                        // this.Fire fires 'Component' events
+                        this._state.Merge({ headerToggled: !this._state.Root.Value.headerToggled });
                         this.Fire("headerClick");
                     }
                 },
-                // dynamic text binding
-                // sets the text of the element
-                // this.Data contains data passed from the parent 'Component'
                 text: () => `${this.Data.title} - ${this.HeaderClass}`
             }),
             div({ props: { className: "body" } }, () => 
-                // Templates passed from a parent component are available through
-                // the this.Templates property
                 this.Templates.body()
             )
         ]
     }
 
-    // Component.Destroy to clean up Component
     public Destroy() {
         super.Destroy();
         this._headerClassScope.Destroy();
-        this._store.Destroy();
+        this._state.Destroy();
     }
 
 }
 
-// Convert Component to function to be referenced in other templates
 var myComponent = Component.ToFunction("my-component", null, MyComponent);
 
 class RootComponent extends Component {
@@ -94,9 +63,16 @@ class RootComponent extends Component {
 
     public Template() {
         return [
-            input({ props: () => ({ value: this.InputValue }) }),
+            input({ 
+                props: () => ({
+                    placeholder: "Enter title...",
+                    value: this.InputValue 
+                }), 
+                on: { keyup: (e) => this._state.Merge({ inputValue: e.target.value }) } 
+            }),
             myComponent({
-                data: () => ({ title: this.InputValue })
+                data: () => ({ title: this.InputValue }),
+                on: { headerClick: () => this._state.Merge({ inputValue: "" }) }
             }, {
                 body: () =>
                     div({ text: "passed from RootComponent" })
