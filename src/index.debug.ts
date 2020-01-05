74fb1b93a429b6e00e517bb2c7558033dd4067c3
TypesInCode <landrumjc@gmail.com>
2020-01-05 02:59:09 -0500

    debug

--- src/index.debug.ts
+++ src/index.debug.ts
@@ -1,24 +1,13 @@
 import { StoreSync, Scope } from "./Store";
 
 async function Test() {
-    var store = new StoreSync({}, (val) => val._id);
-    var query = store.Query((reader) => {
-      var obj = reader.Get<{ _id: string, property: string }>("very-unique-id");
-      return obj && obj.property;
-    });
-
-    var setFired = false;
-    query.Watch((val) => {
-      setFired = true;
-    });
-
+    var store = new StoreSync({ first: "test", second: "test" });
+      
     await store.Action(async (reader, writer) => {
-      await writer.Write({ _id: "very-unique-id", property: "property value" });
+      await writer.Update(reader.Root, { first: "changed", second: "changed" });
     });
-    console.log(setFired)
-    console.log(query.Value);
-    store.Destroy();
-    query.Destroy();
+
+    console.log(store.Root.Value.first);
 }
 
 Test();
