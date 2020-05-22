fdf6f2d79b8f1812d57948b7a16edd99c0c6dda4
TypesInCode <landrumjc@gmail.com>
2020-05-22 17:51:03 -0400

    fixed potential issue with stopped callbacks

--- src/Utils/asyncQueue.ts
+++ src/Utils/asyncQueue.ts
@@ -9,10 +9,11 @@ enum AsyncQueueState {
     Running
 }
 
-export class AsyncQueue<T> {
+export class AsyncQueueObject<T> {
     private list: List<AsyncCallback<T>>;
     private state: AsyncQueueState;
     private data: T;
+    private onComplete: Array<{ (data: T): void }>;
 
     public get Running() {
         return this.state === AsyncQueueState.Running;
@@ -21,6 +22,7 @@ export class AsyncQueue<T> {
     constructor() {
         this.list = new List();
         this.state = AsyncQueueState.Idle;
+        this.onComplete = [];
     }
 
     public Add(callback: AsyncCallback<T>) {
@@ -39,10 +41,16 @@ export class AsyncQueue<T> {
     public Stop(): T {
         this.state = AsyncQueueState.Idle;
         this.list.Clear();
+        this.onComplete = [];
         var data = this.data;
         this.data = null;
 
         return data;
+    }
+
+    public OnComplete(callback: {(data: T): void}) {
+        if(callback)
+            this.onComplete.push(callback);
     }
 
     private Continue() {
@@ -55,7 +63,40 @@ export class AsyncQueue<T> {
                 this.data = data;
                 this.Continue();
             }, this.data);
-        else 
+        else {
+            this.onComplete.forEach(cb => cb(this.data));
+            this.onComplete = [];
             this.Stop();
+        }
+    }
+}
+
+export class AsyncQueue<T> {
+    private queue = new AsyncQueueObject<T>();
+
+    public get Running() {
+        return this.queue.Running;
+    }
+
+    public Add(callback: AsyncCallback<T>) {
+        this.queue.Add(callback);
+    }
+
+    public Start(init?: T) {
+        this.queue.Start(init);
+    }
+
+    public Stop(): T {
+        if(this.queue.Running) {
+            var ret = this.queue.Stop();
+            this.queue = new AsyncQueueObject();
+            return ret;
+        }
+        
+        return null;
+    }
+
+    public OnComplete(callback: {(data: T): void}) {
+        this.queue.OnComplete(callback);
     }
 }
