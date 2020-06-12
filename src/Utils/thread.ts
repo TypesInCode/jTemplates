a60a73981f523d313a158f514ab22fb0881161db
TypesInCode <landrumjc@gmail.com>
2020-06-12 19:35:52 -0400

    thread is now functional

--- src/Utils/thread.ts
+++ src/Utils/thread.ts
@@ -1,76 +1,87 @@
 import { List, INode } from "../Utils/list";
 
+interface IThreadWorkerContext {
+    running: boolean;
+    workEndNode: INode<{(): void}>;
+    workList: List<{(): void}>;
+    DoWork: {(): void};
+}
+
 const workTimeMs = 16;
-var threadWorkerContext: ThreadWorker = null;
+var threadWorkerContext: IThreadWorkerContext = null;
 
-class ThreadWorker {
+function Invoke(ctx: IThreadWorkerContext, callback: {(): void}) {
+    var parent = ctx.workEndNode;
+    ctx.workEndNode = ctx.workList.HeadNode;
+    callback();
+    ctx.workEndNode = parent;
+}
 
-    private workEndNode: INode<{(): void}> = null;
-    private running = false;
-    private workList = new List<{(): void}>();
-    private DoWorkBound = this.DoWork.bind(this);
+function DoWork(ctx: IThreadWorkerContext, startTime = Date.now()) {    
+    var parentContext = threadWorkerContext;
+    threadWorkerContext = ctx;
+    
+    var callback: {(): void};
+    while((Date.now() - startTime) < workTimeMs && (callback = ctx.workList.Pop()))
+        Invoke(ctx, callback);
 
-    public Schedule(callback: {(): void}) {
-        this.workList.AddBefore(this.workEndNode, callback);
-        this.Start();
-    }
+    if(ctx.workList.Size === 0)
+        ctx.running = false;
+    else
+        setTimeout(ctx.DoWork, 0);
 
-    private Invoke(callback: {(): void}) {
-        var parent = this.workEndNode;
-        this.workEndNode = this.workList.HeadNode;
-        callback();
-        this.workEndNode = parent;
-    }
+    threadWorkerContext = parentContext;
+}
 
-    private DoWork() {
-        var parentContext = threadWorkerContext;
-        threadWorkerContext = this;
+function CreateContext() {
+    var ctx: IThreadWorkerContext = {
+        running: true,
+        workEndNode: null,
+        workList: new List(),
+        DoWork: null
+    };
+    ctx.DoWork = () => DoWork(ctx);
+    threadWorkerContext = ctx;
+}
 
-        var startTime = Date.now();
-        var callback: {(): void};
-        while((Date.now() - startTime) < workTimeMs && (callback = this.workList.Pop()))
-            this.Invoke(callback);
-        
-        if(this.workList.Size === 0)
-            this.running = false;
-        else
-            this.ScheduleWork();
+var newThread = false;
+function ScheduleContext(callback: {(): void}) {
+    if(newThread && !threadWorkerContext)
+        CreateContext();
 
-        threadWorkerContext = parentContext;
-    }
+    if(!threadWorkerContext)
+        return callback();
+    
+    threadWorkerContext.workList.AddBefore(threadWorkerContext.workEndNode, callback);
+}
 
-    private ScheduleWork() {
-        setTimeout(this.DoWorkBound, 0);
-    }
+function NewThread(callback: {(): void}) {
+    var startTime = Date.now();
+    var parent = threadWorkerContext;
+    newThread = true;
+    threadWorkerContext = null;
+    callback();
+    newThread = false;
+    
+    if(threadWorkerContext)
+        DoWork(threadWorkerContext, startTime);
 
-    private Start() {
-        if(this.running)
-            return;
-
-        this.running = true;
-        this.DoWork();
-    }
-
+    threadWorkerContext = parent;
 }
 
 export function Schedule(callback: () => void) {
-    if(!threadWorkerContext)
-        return callback();
-
-    threadWorkerContext.Schedule(callback);
+    ScheduleContext(callback);
 }
 
 export function Callback<A = void, B = void, C = void, D = void>(callback: (a: A, b: B, c: C, d: D) => void) {
-    return (a?: A, b?: B, c?: C, d?: D) => {
-        if(!threadWorkerContext)
-            return callback(a, b, c, d);
-        
-        threadWorkerContext.Schedule(() => callback(a, b, c, d));
+    return (a?: A, b?: B, c?: C, d?: D) => {        
+        ScheduleContext(() => callback(a, b, c, d));
     }
 }
 
 export function Thread(callback: {(): void}, forceNew = false) {
-    var thread = !forceNew && threadWorkerContext || new ThreadWorker();
-    thread.Schedule(callback);
-    return new Promise<void>(resolve => thread.Schedule(resolve));
+    if((newThread || threadWorkerContext) && !forceNew)
+        ScheduleContext(callback);
+    else 
+        NewThread(callback);
 }
