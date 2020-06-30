26da6dddb1ad7a14bcccca40557a65945145784a
TypesInCode <landrumjc@gmail.com>
2020-06-30 00:42:34 -0400

    removed comments

--- src/Store/Store/store.ts
+++ src/Store/Store/store.ts
@@ -14,7 +14,6 @@ export class Store<T> {
     constructor(init?: T) {
         if(init)
             this.Write(init);
-            // this.observableTree.Write("ROOT", init);
     }
 
     public Action(action: {(root: T, writer: StoreWriter): void}) {
@@ -26,18 +25,12 @@ export class Store<T> {
         this.Action((root, writer) => 
             writer.Write(root, data)
         );
-        // this.observableTree.Write("ROOT", data);
     }
 
     public Merge(data: Partial<T>) {
         this.Action((root, writer) => 
             writer.Merge(root, data)
         );
-        /* if(ObservableProxy.TypeOf(data) === Type.Value)
-            this.observableTree.Write("ROOT", data);
-        else
-            for(var key in data)
-                this.observableTree.Write(`ROOT.${key}`, data[key]); */
     }
 
     public Destroy() {
