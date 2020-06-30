357072fd6aa7f99ff8627d2c621ed67af3eb135b
TypesInCode <landrumjc@gmail.com>
2020-06-30 19:10:04 -0400

    attempt to get typedoc pages working

--- docs/scripts/docHelpers.js
+++ docs/scripts/docHelpers.js
@@ -96,6 +96,7 @@ function ExecuteTsCode() {
     var code = document.getElementById("typescript-code").innerText;
     var js = ts.transpile(code, { target: "ES2017" });
     js = js.replace(/^import.*$/gm, "");
+    console.log(js);
     eval(js);
 }
 
