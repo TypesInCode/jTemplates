e2f3425aa56b5c23c8e77d07c6ac1bf0c6487303
TypesInCode <landrumjc@gmail.com>
2021-01-03 18:17:41 -0500

    rolled back change

--- src/Store/Store/storeAsyncWriter.ts
+++ src/Store/Store/storeAsyncWriter.ts
@@ -40,10 +40,10 @@ export class StoreAsyncWriter {
         var rootPath = proxy.___node.Path;
         var length = array.length;
 
-        await this.diffAsync.UpdatePath(`${rootPath}.${length}`, data);
-        proxy.___node.Push(data);
-        // var diff = await this.diffAsync.DiffPath(`${rootPath}.${length}`, data);
-        // this.ApplyChanges(diff);
+        /* await this.diffAsync.UpdatePath(`${rootPath}.${length}`, data);
+        proxy.___node.Push(data); */
+        var diff = await this.diffAsync.DiffPath(`${rootPath}.${length}`, data);
+        this.ApplyChanges(diff);
     }
 
     public async Splice<T>(source: Array<T> | ObservableProxy, start: number, deleteCount?: number, ...items: Array<T>) {        
@@ -51,13 +51,13 @@ export class StoreAsyncWriter {
         var rootPath = proxy.___node.Path;
 
         var array = this.observableTree.Get<Array<T>>(rootPath);
-        array = array.map(val => val);
+        array = array.slice();
         array.splice(start, deleteCount, ...items);
         
-        await this.diffAsync.UpdatePath(rootPath, array);
-        return proxy.___node.Splice(start, deleteCount, ...items);
-        // var diff = await this.diffAsync.DiffPath(rootPath, array);
-        // this.ApplyChanges(diff);
+        // await this.diffAsync.UpdatePath(rootPath, array);
+        // return proxy.___node.Splice(start, deleteCount, ...items);
+        var diff = await this.diffAsync.DiffPath(rootPath, array);
+        this.ApplyChanges(diff);
     }
 
     private ApplyChanges(diff: IDiffResponse) {
