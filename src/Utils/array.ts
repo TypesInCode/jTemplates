3f44b230e0085f4c7a7d678439584d5ffe6a6914
Jay Landrum <landrumjc@gmail.com>
2024-09-07 00:07:44 -0400

    added ArrayDiff function

--- src/Utils/array.ts
+++ src/Utils/array.ts
@@ -1,13 +1,13 @@
 export function RemoveNulls(array: (unknown | null)[], startIndex = 0) {
   let nullIndex = startIndex;
-  for (; nullIndex < array.length && array[nullIndex] !== null; nullIndex++) {}
+  for (; nullIndex < array.length && array[nullIndex] !== null; nullIndex++) { }
   let notNullIndex = nullIndex + 1;
 
   for (
     ;
     notNullIndex < array.length && array[notNullIndex] === null;
     notNullIndex++
-  ) {}
+  ) { }
 
   while (notNullIndex < array.length) {
     array[nullIndex] = array[notNullIndex];
@@ -18,8 +18,21 @@ export function RemoveNulls(array: (unknown | null)[], startIndex = 0) {
       ;
       notNullIndex < array.length && array[notNullIndex] === null;
       notNullIndex++
-    ) {}
+    ) { }
   }
 
   array.splice(nullIndex);
+}
+
+export function ArrayDiff(source: any[], target: any[]) {
+  if (source === target)
+    return false;
+
+  if (!source || !target || source.length !== target.length)
+    return true;
+
+  let x = 0;
+  for (; x < source.length && source[x] === target[x]; x++) { }
+
+  return x < source.length;
 }
