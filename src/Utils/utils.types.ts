4e43210287f2795ad467abb45c689208672b7eb7
Jay Landrum <landrumjc@gmail.com>
2024-10-26 13:15:08 -0400

    Added RecursivePartial type to represent props object

--- src/Utils/utils.types.ts
+++ src/Utils/utils.types.ts
@@ -1,3 +1,11 @@
 export interface IDestroyable {
     Destroy(): void;
 }
+
+type NonFunctionKeys<T> = {
+    [P in keyof T]: T[P] extends Function ? never : P;
+}[keyof T];
+
+export declare type RecursivePartial<T> = {
+    [P in keyof Pick<T, NonFunctionKeys<T>>]?: T[P] extends Function ? never : T[P] extends object ? RecursivePartial<T[P]> : T[P];
+};
