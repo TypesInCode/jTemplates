f65030936de0c582b8fcb8daa72e6a4a4efbe5be
Jay Landrum <landrumjc@gmail.com>
2025-01-05 10:49:17 -0500

    Added text element function

--- src/DOM/elements.ts
+++ src/DOM/elements.ts
@@ -1,16 +1,20 @@
 import { vNode } from "../Node/vNode";
 
-export const div = vNode.ToFunction('div');
-export const a = vNode.ToFunction('a');
-export const button = vNode.ToFunction<HTMLButtonElement>('button');
-export const h1 = vNode.ToFunction('h1');
-export const input = vNode.ToFunction<HTMLInputElement>('input');
-export const span = vNode.ToFunction('span');
-export const table = vNode.ToFunction('table');
-export const tbody = vNode.ToFunction('tbody');
-export const tr = vNode.ToFunction('tr');
-export const td = vNode.ToFunction('td');
+export const div = vNode.ToFunction("div");
+export const a = vNode.ToFunction("a");
+export const button = vNode.ToFunction<HTMLButtonElement>("button");
+export const h1 = vNode.ToFunction("h1");
+export const input = vNode.ToFunction<HTMLInputElement>("input");
+export const span = vNode.ToFunction("span");
+export const table = vNode.ToFunction("table");
+export const tbody = vNode.ToFunction("tbody");
+export const tr = vNode.ToFunction("tr");
+export const td = vNode.ToFunction("td");
 
+const textElement = vNode.ToFunction("text");
+export const text = function (callback: () => string) {
+  return textElement({ props: () => ({ nodeValue: callback() }) });
+};
 
 // import { ElementNode } from "../Node/elementNode";
 // import { ElementNodeFunctionParam, ElementChildrenFunctionParam } from "../Node/elementNode.types";
