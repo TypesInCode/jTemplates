c6ee34f0fcf0bc1a18f31904866c1a2f6b9ec10f
Jay Landrum <jay.landrum@typesincode.com>
2025-08-25 05:51:42 -0400

    Added special cases for lengths of 0 and 1

--- src/Utils/distinctArray.ts
+++ src/Utils/distinctArray.ts
@@ -1,6 +1,6 @@
 export type DistinctArray<T> = {
     id: (value: T) => number;
-    distinct: boolean[];
+    distinct: Array<true | undefined> | null;
     array: T[];
 }
 
@@ -8,17 +8,31 @@ export namespace DistinctArray {
     export function Create<T>(id: (value: T) => number): DistinctArray<T> {
         return {
             id,
-            distinct: [],
+            distinct: null,
             array: [] as T[]
         };
     }
 
-    export function Push<T>({ id, distinct, array }: DistinctArray<T>, value: T) {
-        const vId = id(value);
+    export function Push<T>(distinctArr: DistinctArray<T>, value: T) {
 
-        if(!distinct[vId]) {
-            distinct[vId] = true;
-            array.push(value);
+        const{ id, array }: DistinctArray<T> = distinctArr;
+        switch(array.length) {
+            case 0:
+                array.push(value);
+                break;
+            case 1: {
+                if(distinctArr.distinct === null) {
+                    distinctArr.distinct = []
+                    distinctArr.distinct[id(array[0])] = true;
+                }
+            }
+            default: {
+                const vId = id(value);
+                if(distinctArr.distinct[vId] === undefined) {
+                    distinctArr.distinct[vId] = true;
+                    array.push(value);
+                }
+            }
         }
     }
 
