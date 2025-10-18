10a82a24bebfa6f73250b4b9efb25e8a772969f8
Jay Landrum <jay.landrum@typesincode.com>
2025-10-17 21:15:33 -0400

    Added AI unit tests

--- test/observable/observableNode-spec.ts
+++ test/observable/observableNode-spec.ts
@@ -1,284 +1,121 @@
-/* import { expect } from 'chai';
+import { expect } from 'chai';
 import 'mocha';
-import { CreateProxy } from '../../src/Store/Tree/observableNode';
-import { ObservableScope } from '../../src/Store/Tree/observableScope';
+import { ObservableNode } from '../../src/Store/Tree/observableNode';
 
 describe("Observable Node", () => {
-    it('Basic Test', () => {
-        const proxy = CreateProxy({ test: "value" });
+    it('Create - Basic Object', () => {
+        const proxy = ObservableNode.Create({ test: "value" });
         expect(proxy.test).to.eq("value");
     });
-    it('Basic Update', () => {
-        const proxy = CreateProxy({ test: "value" });
-        proxy.Set("test", "changed");
-        expect(proxy.test).to.eq("value");
-        proxy.Update();
-        expect(proxy.test).to.eq("changed");
-    });
-    it('Basic Scope', () => {
-        const proxy = CreateProxy({ test: "value" });
-        const scope = ObservableScope.Create(() => proxy.test);
-        proxy.Set("test", "changed");
-        expect(ObservableScope.Value(scope)).to.eq("value");
-        proxy.Update();
-        expect(ObservableScope.Value(scope)).to.eq("changed");
-    });
-    it('Child Object', () => {
-        const proxy = CreateProxy({ child: { test: "value" } });
-        const scope_pre = ObservableScope.Create(() => proxy.child);
-        const scope = ObservableScope.Create(() => ObservableScope.Value(scope_pre).test);
-        ObservableScope.Watch(scope_pre, (scope) => console.log("scope_pre", ObservableScope.Value(scope)));
-        proxy.child.Set("test", "changed");
-        expect(ObservableScope.Value(scope)).to.eq("value");
-        proxy.child.Update();
-        expect(ObservableScope.Value(scope)).to.eq("changed");
-        proxy.Set("child", { test: "changed again" });
-        proxy.Update();
-        expect(ObservableScope.Value(scope)).to.eq("changed again");
-    });
-    it('Copy Child Object', () => {
-        const proxy = CreateProxy({ child: { test: "value", other: "other" } });
-        const scope = ObservableScope.Create(() => proxy.child.test);
-        const copy = { ...proxy.child };
-        copy.test = "changed";
-        proxy.Set("child", copy);
-        expect(ObservableScope.Value(scope)).to.eq("value");
-        proxy.Update();
-        expect(ObservableScope.Value(scope)).to.eq("changed");
-    });
-    it('Array Update', () => {
-        const proxy = CreateProxy(["test1", "test2"]);
-        expect(proxy[1]).to.eq("test2");
-        const scope = ObservableScope.Create(() => proxy.length);
-        proxy.Set(2, "changed");
-        expect(ObservableScope.Value(scope)).to.eq(2);
-        proxy.Update();
-        expect(ObservableScope.Value(scope)).to.eq(3);
-    });
-    it('Array Splice', () =import { expect } from 'chai';
-    import 'mocha';
-    import { CreateProxy } from '../../src/Store/Tree/observableNode';
-    import { ObservableScope } from '../../src/Store/Tree/observableScope';
 
-    describe("Observable Node", () => {
-        it('Basic Test', () => {
-            const proxy = CreateProxy({ test: "value" });
-            expect(proxy.test).to.eq("value");
-        });
-        it('Basic Update', () => {
-            const proxy = CreateProxy({ test: "value" });
-            proxy.Set("test", "changed");
-            expect(proxy.test).to.eq("value");
-            proxy.Update();
-            expect(proxy.test).to.eq("changed");
-        });
-        it('Basic Scope', () => {
-            const proxy = CreateProxy({ test: "value" });
-            const scope = ObservableScope.Create(() => proxy.test);
-            proxy.Set("test", "changed");
-            expect(ObservableScope.Value(scope)).to.eq("value");
-            proxy.Update();
-            expect(ObservableScope.Value(scope)).to.eq("changed");
-        });
-        it('Child Object', () => {
-            const proxy = CreateProxy({ child: { test: "value" } });
-            const scope_pre = ObservableScope.Create(() => proxy.child);
-            const scope = ObservableScope.Create(() => ObservableScope.Value(scope_pre).test);
-            ObservableScope.Watch(scope_pre, (scope) => console.log("scope_pre", ObservableScope.Value(scope)));
-            proxy.child.Set("test", "changed");
-            expect(ObservableScope.Value(scope)).to.eq("value");
-            proxy.child.Update();
-            expect(ObservableScope.Value(scope)).to.eq("changed");
-            proxy.Set("child", { test: "changed again" });
-            proxy.Update();
-            expect(ObservableScope.Value(scope)).to.eq("changed again");
-        });
-        it('Copy Child Object', () => {
-            const proxy = CreateProxy({ child: { test: "value", other: "other" } });
-            const scope = ObservableScope.Create(() => proxy.child.test);
-            const copy = { ...proxy.child };
-            copy.test = "changed";
-            proxy.Set("child", copy);
-            expect(ObservableScope.Value(scope)).to.eq("value");
-            proxy.Update();
-            expect(ObservableScope.Value(scope)).to.eq("changed");
-        });
-        it('Array Update', () => {
-            const proxy = CreateProxy(["test1", "test2"]);
-            expect(proxy[1]).to.eq("test2");
-            const scope = ObservableScope.Create(() => proxy.length);
-            proxy.Set(2, "changed");
-            expect(ObservableScope.Value(scope)).to.eq(2);
-            proxy.Update();
-            expect(ObservableScope.Value(scope)).to.eq(3);
-        });
-        it('Array Splice', () => {
-            const proxy = CreateProxy(["test1", "test2"]);
-            const scope = ObservableScope.Create(() => proxy.length);
-            const firstScope = ObservableScope.Create(() => proxy[1]);
-            ObservableScope.Watch(firstScope, (scope) => console.log("scope", ObservableScope.Value(scope)));
-            expect(ObservableScope.Value(firstScope)).to.eq("test2");
-            proxy.splice(0, 1);
-            expect(ObservableScope.Value(scope)).to.eq(1);
-            expect(ObservableScope.Value(firstScope)).to.eq(undefined);
-            proxy.splice(1, 0, "test3", "test4");
-            expect(ObservableScope.Value(scope)).to.eq(3);
-            expect(ObservableScope.Value(firstScope)).to.eq("test3");
-        });
-        it('Array with objects', () => {
-            const rootProxy = CreateProxy([{ value: "first" }, { value: "second" }, { value: "third" }]);
-            expect(rootProxy[0].value).to.eq("first");
-            rootProxy[0].Set("value", "first changed");
-            rootProxy[0].Update();
-            rootProxy[1].Set("value", "second hidden changed");
-            rootProxy[1].Update();
-            rootProxy.Set(1, { value: "second changed" });
-            const json = rootProxy.toJSON();
-            expect(json[0].value).to.eq("first changed");
-            expect(json[1].value).to.eq("second changed");
-            expect(rootProxy[1].value).to.eq("second hidden changed");
-            rootProxy.Update();
-            expect(rootProxy[1].value).to.eq("second changed");
-        }),
-        it('Diff Object Basic Update', () => {
-            const proxy = CreateProxy({ test: "value" }, true);
-            proxy.Set("test", "changed");
-            expect(proxy.test).to.eq("value");
-            proxy.Update();
-            expect(proxy.test).to.eq("changed");
-        }),
-        it('Diff Object Basic Update 2', () => {
-            const proxy = CreateProxy({ test: "value", prop: "other" }, true);
-            proxy.Set("test", "changed");
-            expect(proxy.test).to.eq("value");
-            proxy.Update();
-            expect(proxy.test).to.eq("changed");
-        }),
-        it('Diff Object Child Update', () => {
-            const proxy = CreateProxy({ test: { child: "value" }, prop: "other" }, true);
-            proxy.Set("test", { child: "changed"});
-            expect(proxy.test.child).to.eq("value");
-            proxy.Update();
-            expect(proxy.test.child).to.eq("changed");
-        }),
-        it('Diff Object Child Update 2', () => {
-            const proxy = CreateProxy({ test: { child: "value", prop: "other" }, prop: "other" }, true);
-            proxy.Set("test", { child: "changed", prop: "other" });
-            expect(proxy.test.child).to.eq("value");
-            proxy.Update();
-            expect(proxy.test.child).to.eq("changed");
-        }),
-        it('Diff Object Child Update 2.1', () => {
-            const proxy = CreateProxy({ test: { child: "value", prop: "other" }, prop: "other" }, true);
-            expect(proxy.test.prop).to.eq("other");
-            proxy.Set("test", { child: "changed", prop: "other" });
-            proxy.Update();
-            expect(proxy.test.child).to.eq("changed");
-        }),
-        it('Diff Sliced Array', () => {
-            const proxy = CreateProxy(["test1", "test2"], true);
-            expect(proxy[0]).to.eq("test1");
-            proxy.splice(0, 1);
-            proxy.Update();
-            expect(proxy[0]).to.eq("test2");
-        })
-    });
-
-    // root -> dependecies (...) -> output.subscribe()
-    // root -> dependencies (...) -> output.subscribe()
-
-    // name$ //gets the .name property
-    // description$ //get the .description
-
-    // { bigState: { leftState: any, rightState: any } }
-
-    // state = { bigState } // did I actually change anything?
-
-    // const state = state.bigState
-
-    // const state.leftState
-> {
-        const proxy = CreateProxy(["test1", "test2"]);
-        const scope = ObservableScope.Create(() => proxy.length);
-        const firstScope = ObservableScope.Create(() => proxy[1]);
-        ObservableScope.Watch(firstScope, (scope) => console.log("scope", ObservableScope.Value(scope)));
-        expect(ObservableScope.Value(firstScope)).to.eq("test2");
-        proxy.splice(0, 1);
-        expect(ObservableScope.Value(scope)).to.eq(1);
-        expect(ObservableScope.Value(firstScope)).to.eq(undefined);
-        proxy.splice(1, 0, "test3", "test4");
-        expect(ObservableScope.Value(scope)).to.eq(3);
-        expect(ObservableScope.Value(firstScope)).to.eq("test3");
-    });
-    it('Array with objects', () => {
-        const rootProxy = CreateProxy([{ value: "first" }, { value: "second" }, { value: "third" }]);
-        expect(rootProxy[0].value).to.eq("first");
-        rootProxy[0].Set("value", "first changed");
-        rootProxy[0].Update();
-        rootProxy[1].Set("value", "second hidden changed");
-        rootProxy[1].Update();
-        rootProxy.Set(1, { value: "second changed" });
-        const json = rootProxy.toJSON();
-        expect(json[0].value).to.eq("first changed");
-        expect(json[1].value).to.eq("second changed");
-        expect(rootProxy[1].value).to.eq("second hidden changed");
-        rootProxy.Update();
-        expect(rootProxy[1].value).to.eq("second changed");
-    }),
-    it('Diff Object Basic Update', () => {
-        const proxy = CreateProxy({ test: "value" }, true);
-        proxy.Set("test", "changed");
-        expect(proxy.test).to.eq("value");
-        proxy.Update();
-        expect(proxy.test).to.eq("changed");
-    }),
-    it('Diff Object Basic Update 2', () => {
-        const proxy = CreateProxy({ test: "value", prop: "other" }, true);
-        proxy.Set("test", "changed");
-        expect(proxy.test).to.eq("value");
-        proxy.Update();
-        expect(proxy.test).to.eq("changed");
-    }),
-    it('Diff Object Child Update', () => {
-        const proxy = CreateProxy({ test: { child: "value" }, prop: "other" }, true);
-        proxy.Set("test", { child: "changed"});
-        expect(proxy.test.child).to.eq("value");
-        proxy.Update();
-        expect(proxy.test.child).to.eq("changed");
-    }),
-    it('Diff Object Child Update 2', () => {
-        const proxy = CreateProxy({ test: { child: "value", prop: "other" }, prop: "other" }, true);
-        proxy.Set("test", { child: "changed", prop: "other" });
-        expect(proxy.test.child).to.eq("value");
-        proxy.Update();
-        expect(proxy.test.child).to.eq("changed");
-    }),
-    it('Diff Object Child Update 2.1', () => {
-        const proxy = CreateProxy({ test: { child: "value", prop: "other" }, prop: "other" }, true);
-        expect(proxy.test.prop).to.eq("other");
-        proxy.Set("test", { child: "changed", prop: "other" });
-        proxy.Update();
-        expect(proxy.test.child).to.eq("changed");
-    }),
-    it('Diff Sliced Array', () => {
-        const proxy = CreateProxy(["test1", "test2"], true);
+    it('Create - Basic Array', () => {
+        const proxy = ObservableNode.Create(["test1", "test2"]);
         expect(proxy[0]).to.eq("test1");
-        proxy.splice(0, 1);
-        proxy.Update();
-        expect(proxy[0]).to.eq("test2");
-    })
-}); */
+        expect(proxy[1]).to.eq("test2");
+    });
 
-// root -> dependecies (...) -> output.subscribe()
-// root -> dependencies (...) -> output.subscribe()
+    it('Create - Nested Object', () => {
+        const proxy = ObservableNode.Create({ 
+            child: { 
+                test: "value" 
+            } 
+        });
+        expect(proxy.child.test).to.eq("value");
+    });
 
-// name$ //gets the .name property
-// description$ //get the .description
+    it('ApplyDiff - Basic Object Update', () => {
+        const proxy = ObservableNode.Create({ test: "value" });
+        const diffResult = [
+            {
+                path: ["test"],
+                value: "changed"
+            }
+        ];
+        
+        // Apply the diff
+        ObservableNode.ApplyDiff(proxy, diffResult);
+        
+        // Value should be updated
+        expect(proxy.test).to.eq("changed");
+    });
 
-// { bigState: { leftState: any, rightState: any } }
+    it('ApplyDiff - Nested Object Update', () => {
+        const proxy = ObservableNode.Create({ 
+            child: { 
+                test: "value" 
+            } 
+        });
+        const diffResult = [
+            {
+                path: ["child", "test"],
+                value: "changed"
+            }
+        ];
+        
+        // Apply the diff
+        ObservableNode.ApplyDiff(proxy, diffResult);
+        
+        // Value should be updated
+        expect(proxy.child.test).to.eq("changed");
+    });
 
-// state = { bigState } // did I actually change anything?
+    it('CreateFactory - With Alias Function', () => {
+        // Create an alias function that maps objects to a specific property
+        const aliasFn = (value: any) => {
+            if (value && typeof value === 'object' && 'id' in value) {
+                return { aliased: value.id };
+            }
+            return undefined;
+        };
+        
+        const factory = ObservableNode.CreateFactory(aliasFn);
+        const proxy = factory({ id: "test-id", name: "test-name" });
+        
+        // Should create proxy from the aliased object
+        expect((proxy as any).aliased).to.eq("test-id");
+    });
 
-// const state = state.bigState
+    it('CreateFactory - With Alias Function and Nested Structure', () => {
+        // Create an alias function that maps objects to a nested property
+        const aliasFn = (value: any) => {
+            if (value && typeof value === 'object' && 'data' in value) {
+                return { 
+                    wrapper: {
+                        id: value.data.id
+                    }
+                };
+            }
+            return undefined;
+        };
+        
+        const factory = ObservableNode.CreateFactory(aliasFn);
+        const proxy = factory({ data: { id: "nested-id", name: "test" } });
+        
+        // Should create proxy from the aliased object with nested structure
+        expect((proxy as any).wrapper.id).to.eq("nested-id");
+    });
 
-// const state.leftState
+    it('Create - With Primitives', () => {
+        const strProxy = ObservableNode.Create("string");
+        const numProxy = ObservableNode.Create(42);
+        const boolProxy = ObservableNode.Create(true);
+        
+        expect(strProxy).to.eq("string");
+        expect(numProxy).to.eq(42);
+        expect(boolProxy).to.eq(true);
+    });
+
+    it('Create - Complex Nested Structure', () => {
+        const proxy = ObservableNode.Create({
+            level1: {
+                level2: {
+                    value: "deep"
+                },
+                array: [1, 2, 3]
+            }
+        });
+        
+        expect(proxy.level1.level2.value).to.eq("deep");
+        expect(proxy.level1.array[0]).to.eq(1);
+    });
+});
