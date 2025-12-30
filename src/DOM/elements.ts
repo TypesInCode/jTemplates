336d59478a7a38cd7b222795cf5818c43c3c94ed
Jay Landrum <landrumjc@gmail.com>
2025-12-30 18:38:33 -0500

    Updated version number. Added missing elements. Updated utils export.

--- src/DOM/elements.ts
+++ src/DOM/elements.ts
@@ -5,6 +5,8 @@ export const a = vNode.ToFunction("a");
 export const b = vNode.ToFunction("b");
 export const button = vNode.ToFunction<HTMLButtonElement>("button");
 export const h1 = vNode.ToFunction("h1");
+export const h2 = vNode.ToFunction("h2");
+export const h3 = vNode.ToFunction("h3");
 export const input = vNode.ToFunction<HTMLInputElement>("input");
 export const textarea = vNode.ToFunction<HTMLTextAreaElement>("textarea");
 export const span = vNode.ToFunction("span");
@@ -14,6 +16,11 @@ export const tr = vNode.ToFunction("tr");
 export const td = vNode.ToFunction("td");
 export const img = vNode.ToFunction<HTMLImageElement>("img");
 export const video = vNode.ToFunction<HTMLVideoElement>("video");
+export const select = vNode.ToFunction<HTMLSelectElement>("select");
+export const option = vNode.ToFunction<HTMLOptionElement>("option");
+export const aside = vNode.ToFunction("aside");
+export const p = vNode.ToFunction("p");
+export const label = vNode.ToFunction("label");
 
 const textElement = vNode.ToFunction("text");
 export const text = function (callback: () => string) {
