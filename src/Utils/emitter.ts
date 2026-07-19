1a9c74ab1024887679b7db92180a6ad6262858a3
Jay Landrum <jay.landrum@typesincode.com>
2026-07-19 15:23:16 -0400

    Added deferred compact process

--- src/Utils/emitter.ts
+++ src/Utils/emitter.ts
@@ -4,8 +4,70 @@ export type EmitterCallback<T extends readonly any[] = any[]> = (
 export type Emitter = EmitterCallback[]; // [number, ...EmitterCallback[]];
 
 export namespace Emitter {
+  const destroyed = new WeakSet();
+  let pendingCompactEmitters: Emitter[] = [];
+
   export function Create(): Emitter {
     return [];
+  }
+
+  const scheduleCallback =
+    typeof requestIdleCallback !== "undefined"
+      ? requestIdleCallback
+      : setTimeout;
+
+  export function Compact(emitter: Emitter) {
+    if (emitter.length === 0) return;
+
+    pendingCompactEmitters.push(emitter);
+    ScheduleCompact();
+  }
+
+  let compactScheduled = false;
+  function ScheduleCompact() {
+    if (compactScheduled) return;
+
+    compactScheduled = true;
+    scheduleCallback(PerformCompact);
+  }
+
+  function PerformCompact() {
+    compactScheduled = false;
+
+    let compactEmitters = pendingCompactEmitters;
+    pendingCompactEmitters = [];
+
+    const processedEmitters = new Set<Emitter>();
+
+    for (let x = 0; x < compactEmitters.length; x++)
+      if (!processedEmitters.has(compactEmitters[x])) {
+        CompactEmitter(compactEmitters[x]);
+        processedEmitters.add(compactEmitters[x]);
+      }
+  }
+
+  function CompactEmitter(emitter: Emitter) {
+    let count = 0;
+    for (
+      ;
+      count < emitter.length &&
+      (emitter[count] === null || destroyed.has(emitter[count]));
+      count++
+    ) {}
+
+    if (count > 0) emitter.splice(0, count);
+
+    let writePos = 0;
+    for (let x = 0; x < emitter.length; x++) {
+      if (emitter[x] !== null && !destroyed.has(emitter[x]))
+        emitter[writePos++] = emitter[x];
+    }
+
+    if (writePos < emitter.length) emitter.splice(writePos);
+  }
+
+  export function DestroyCallback(callback: EmitterCallback) {
+    destroyed.add(callback);
   }
 
   export function On(emitter: Emitter, callback: EmitterCallback) {
@@ -15,7 +77,7 @@ export namespace Emitter {
   export function Emit(emitter: Emitter, ...args: any[]) {
     let writePos = 0;
     for (let x = 0; x < emitter.length; x++) {
-      if (emitter[x] !== null) {
+      if (emitter[x] !== null && !destroyed.has(emitter[x])) {
         (emitter[x] as EmitterCallback)(...args);
         emitter[writePos++] = emitter[x];
       }
@@ -26,10 +88,15 @@ export namespace Emitter {
 
   export function Remove(emitter: Emitter, callback: EmitterCallback) {
     const index = emitter.indexOf(callback);
-    if (index >= 0) emitter[index] = null;
+    if (index >= 0) {
+      emitter[index] = null;
+      Compact(emitter);
+    }
   }
 
   export function Clear(emitter: Emitter) {
+    if (emitter.length === 0) return;
+
     emitter.splice(0);
   }
 }
